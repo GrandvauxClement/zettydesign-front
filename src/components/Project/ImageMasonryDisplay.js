29f9318479e6e5bbf5ba16949915f9ec14257c14
GrandvauxClement <clement.grandvaux@hotmail.com>
2022-02-12 11:31:13 +0100

    begin modal project

--- src/components/Project/ImageMasonryDisplay.js
+++ src/components/Project/ImageMasonryDisplay.js
@@ -4,6 +4,8 @@ import Stack from '@mui/material/Stack';
 import Masonry from '@mui/lab/Masonry';
 import Api from "../../api";
 import styled from "@mui/material/styles/styled";
+import {useState} from "react";
+import ModalProject from "./modalProject";
 
 const PREFIX = 'Project';
 
@@ -18,10 +20,10 @@ const Root = styled('div')(({theme}) => ({
     },
     [`& .${classes.image}`]: {
         transition: "all 1s ease",
-        background: "#f1f1f1",
+        borderBottomLeftRadius: 4,
+        borderBottomRightRadius: 4,
         '&:hover':{
             cursor: "pointer",
-            background: "#f00",
             transform:"scale(1.25)",
         }
     }
@@ -29,22 +31,29 @@ const Root = styled('div')(({theme}) => ({
 
 export default function ImageMasonry({projects}) {
     const urlImage = Api.baseUrl + 'public/images/projets/';
+    const [open, setOpen] = useState(false);
+    const handleOpen = () => setOpen(true);
+    const handleClose = () => setOpen(false);
+
     return (
         <Root className={classes.root}>
             <Masonry columns={3} spacing={1}>
                 {projects.map((item, index) => (
-                    <Stack key={index}>
-                        <img
-                            src={`${urlImage}${item.principalImage}`}
-                            srcSet={`${urlImage}${item.principalImage}`}
-                            alt={item.title}
-                            loading="lazy"
-                            className={classes.image}
-                          //  style={{ borderBottomLeftRadius: 4, borderBottomRightRadius: 4 }}
-                        />
-                    </Stack>
+                    <>
+                        <Stack key={index} onClick={handleOpen}>
+                            <img
+                                src={`${urlImage}${item.principalImage}`}
+                                srcSet={`${urlImage}${item.principalImage}`}
+                                alt={item.title}
+                                loading="lazy"
+                                className={classes.image}
+                            />
+                        </Stack>
+                        <ModalProject open={open} onClose={handleClose} project={item}/>
+                    </>
                 ))}
             </Masonry>
+
         </Root>
     );
 }
