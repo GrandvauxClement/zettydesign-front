d4d439417f63e943199b350fcdfff309417cac61
GrandvauxClement <clement.grandvaux@hotmail.com>
2022-03-14 12:07:02 +0100

    add BackOffice container

---
+++ src/components/BackOffice/BackOfficeContainer.js
@@ -0,0 +1,38 @@
+import React, {useEffect, useState} from "react";
+import UserService from "../../services/user.service";
+import {useNavigate} from "react-router-dom";
+import {Box} from "@mui/material";
+import CircularProgress from "@mui/material/CircularProgress";
+import IndexBackOffice from "./index";
+
+const BackOfficeContainer = () => {
+    const navigate = useNavigate();
+    const [checkToken, setCheckToken] = useState(false)
+
+    useEffect(()=> {
+        UserService.checkToken().then((res) => {
+            console.log(res);
+            if (res.data.isLoggedIn){
+                setCheckToken(true);
+            } else {
+                navigate("/");
+            }
+
+        })
+    }, [setCheckToken])
+
+    if(checkToken){
+        return (
+            <IndexBackOffice />
+        )
+    } else {
+        return (
+            <Box sx={{display: 'flex', alignItems: 'center', mt: 3}}>
+                <CircularProgress/>
+            </Box>
+        )
+    }
+
+}
+
+export default BackOfficeContainer
